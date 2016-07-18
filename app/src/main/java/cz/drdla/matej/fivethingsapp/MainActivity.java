6d04df838113e23d31cc7193d43436383531fdbd
drdlama1 <matej.drdla@uhk.cz>
2016-07-18 14:46:49 +0800

    Main function done (working index with all 5 pages)!

--- app/src/main/java/cz/drdla/matej/fivethingsapp/MainActivity.java
+++ app/src/main/java/cz/drdla/matej/fivethingsapp/MainActivity.java
@@ -10,12 +10,20 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
         mainMenu();
     }
 
@@ -24,8 +32,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), PageOne.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(getApplicationContext(), PageOne.class);
+                startActivity(intent1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(getApplicationContext(), PageTwo.class);
+                startActivity(intent2);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(getApplicationContext(), PageThree.class);
+                startActivity(intent3);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
+                Intent intent4 = new Intent(getApplicationContext(), PageFour.class);
+                startActivity(intent4);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
+                Intent intent5 = new Intent(getApplicationContext(), PageFive.class);
+                startActivity(intent5);
             }
         });
     }
---
+++ app/src/main/java/cz/drdla/matej/fivethingsapp/PageThree.java
@@ -0,0 +1,17 @@
+package cz.drdla.matej.fivethingsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matt on 7/18/16.
+ */
+public class PageThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_three);
+    }
+
+}
