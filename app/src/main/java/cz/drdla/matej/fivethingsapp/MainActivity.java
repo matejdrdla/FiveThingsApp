23a28d49f6e1117539329602e572dd914940412f
drdlama1 <matej.drdla@uhk.cz>
2016-07-18 14:36:17 +0800

    First page working

--- app/src/main/java/cz/drdla/matej/fivethingsapp/MainActivity.java
+++ app/src/main/java/cz/drdla/matej/fivethingsapp/MainActivity.java
@@ -1,13 +1,32 @@
 package cz.drdla.matej.fivethingsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.btn1);
+        mainMenu();
+    }
+
+    public void mainMenu() {
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nevim", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), PageOne.class);
+                startActivity(intent);
+            }
+        });
     }
 }
